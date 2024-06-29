379c4d5f937ad83c23bbde70c848075b35fc7d23
PixelBoot <bootingpixels@gmail.com>
2024-06-29 22:46:56 +0200

    sunfish: Enable Face Unlock

--- aosp_sunfish.mk
+++ aosp_sunfish.mk
@@ -41,9 +41,10 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_product.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/handheld_vendor.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/telephony_vendor.mk)
 
-# Inherit some common PixelExperience stuff
+# Inherit some common PixelBuilds stuff
 TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_GAPPS_ARCH := arm64
+TARGET_FACE_UNLOCK_SUPPORTED := true
 $(call inherit-product, vendor/pb/config/common_full_phone.mk)
 
 $(call inherit-product, device/google/sunfish/device-sunfish.mk)
