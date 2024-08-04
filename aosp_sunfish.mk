43562dc44391afbd8f77d73d1c4c52a7c3510b8c
tnakamur <taka.nakamura1009@gmail.com>
2024-08-04 12:19:40 +0900

    delete vendor/pb

--- aosp_sunfish.mk
+++ aosp_sunfish.mk
@@ -45,7 +45,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/telephony_vendor.mk)
 TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_GAPPS_ARCH := arm64
 TARGET_FACE_UNLOCK_SUPPORTED := true
-$(call inherit-product, vendor/pb/config/common_full_phone.mk)
+#$(call inherit-product, vendor/pb/config/common_full_phone.mk)
 
 $(call inherit-product, device/google/sunfish/device-sunfish.mk)
 $(call inherit-product-if-exists, vendor/google_devices/sunfish/proprietary/device-vendor.mk)
