786cccec563f63e693056606ffd187c2bfaf9d4d
Khaliq <61682259+khaliq8145@users.noreply.github.com>
2024-12-05 02:46:59 +0700

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,8 +15,10 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/aosp_sunfish.mk \
-    $(LOCAL_DIR)/aosp_sunfish_hwasan.mk \
+    $(LOCAL_DIR)/pixelstar_sunfish.mk \
 
 COMMON_LUNCH_CHOICES := \
-    aosp_sunfish-trunk_staging-userdebug \
+    pixelstar_sunfish-trunk_staging-userdebug \
+    pixelstar_sunfish-eng \
+    pixelstar_sunfish-user \
+    pixelstar_sunfish-userdebug \
